1c7294d528c6e7ca2eab6b42498a7373cc609495
Faizanabid1998 <faizanabid1998@gmail.com>
2023-02-19 23:00:56 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alerts from './components/Alerts';
+// import React from "react";
+// import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Router, Route } from 'react-router-dom';
+
 
 function App() {
+  const [mode, setMode]=useState('light');
+  const [alert, setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleMode=()=>{
+    if (mode ==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#042743';
+      showAlert('Dark mode has been enabled','success');
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert('Light mode has been enabled','success');
+    }
+  }
+
+
   return (
   <>
-      <Navbar navTitle="Textutils" About="About textUtils"/>
+  <Router>
+      <Navbar navTitle="Textutils" About="About textUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alerts alert={alert}/>
      <div className="container">
-     <TextForm heading="Enter the text to analyze"/>
+     
+     <Routes>
+          <Route path="/about" element={<About />}/>
+            
+
+          <Route path="/" element={   <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>
+}/>
+ 
+        </Routes>
       </div>
+      </Router>
+      
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,10 +5,17 @@ export default function TextForm(props) {
   const handleClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to upper case", "success");
   };
   const handleloClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lower case", "success");
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared", "success");
   };
 
   const handleChange = (event) => {
@@ -17,7 +24,10 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <div className="mb-3">
           <h1> {props.heading} </h1>
 
@@ -27,6 +37,10 @@ export default function TextForm(props) {
             rows="8"
             value={text}
             onChange={handleChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleClick}>
@@ -35,9 +49,15 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleloClick}>
           Convert to lower case
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear text
+        </button>
       </div>
 
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h1 className="my-3">Summary</h1>
         <p>
           Your text has {text.split(" ").length} words and {text.length}{" "}
@@ -50,8 +70,12 @@ export default function TextForm(props) {
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="8"
-            value={text}
+            value={text.length > 0 ? text : "Enter some text to display"}
             onChange={handleChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
           ></textarea>
         </div>
       </div>
